fd292a76d91ecaf0b9f676b7bc76cea348c78784
YoungCatinho <nk-97@t-online.de>
2019-10-22 16:13:02 +0200

    Kleine Änderungen

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Niklas erste Flutter-App'),
+      home: MyHomePage(title: 'Niklas erste FlutterApp'),
     );
   }
 }
