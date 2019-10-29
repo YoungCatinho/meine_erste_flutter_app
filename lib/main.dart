6cc75aa37014fd2309b4ee4a67652bd5e8cd2b3b
YoungCatinho <nk-97@t-online.de>
2019-10-29 13:45:58 +0100

    Meine App

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 
-
 import 'package:flutter/material.dart';
+
+const String path = '/Users/niklaskuemmel/Desktop/Androidprog/Widgetpräsentation/icon.png';
+var title = "Title...";
+var counter = 0;
 
 void main() => runApp(MyApp());
 
@@ -28,18 +31,46 @@ class _SeiteVonNiklasState extends State<SeiteVonNiklas> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Seite von Niklas"),
+        title: Text("App mit ListTile"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Image.network("https://www.google.de/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwi-5vSXkrDlAhWR66QKHUEBB4MQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DpWkzec7OfVo&psig=AOvVaw0e7OQFr-enbiNs6vHWNL_3&ust=1571843145946069"),
-            reiheMachen("Name:", "Niklas"),
-            reiheMachen("Alter:", "21"),
-            reiheMachen("Wohnort", "Petersberg")
-          ],
-        ),
+      body: ListTile(
+          title: Text("Title"),
+          subtitle: Text("Subtitle..."),
+          enabled: true,
+          leading: Text("Leading..."),
+          trailing: Text("Counter: " + counter.toString()),
+        onTap: () {
+            setState(() {
+              counter++;
+            });
+        },
+        onLongPress: () {
+            setState(() {
+              counter = 0;
+            });
+        },
       ),
+
+
+//      SafeArea(
+//        child: ListView(
+//          children: List.generate(100, (i) => Text('Hier steht Text.', style: TextStyle(fontSize: 30),)),
+//        ),
+//        top: true,
+//        bottom: true,
+//        left: true,
+//        right: true,
+//      )
+      //SingleChildScrollView(
+      //child: Column(
+      //children: <Widget>[
+      //Image.network("https://www.google.de/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwi-5vSXkrDlAhWR66QKHUEBB4MQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DpWkzec7OfVo&psig=AOvVaw0e7OQFr-enbiNs6vHWNL_3&ust=1571843145946069"),
+      //reiheMachen("Name:", "Niklas"),
+      //reiheMachen("Alter:", "21"),
+      //reiheMachen("Wohnort", "Petersberg"),
+      //],
+      //),
+      //),
     );
   }
 
@@ -63,4 +94,10 @@ class _SeiteVonNiklasState extends State<SeiteVonNiklas> {
       ),
     );
   }
+}
+
+Widget img() {
+  var assetsImage = new AssetImage('assets/icon.png');
+  var icon = new Image(image: assetsImage, width: 40.0, height: 40.0);
+  return icon;
 }
